7de06fccbcf499b8e6ad1090d0beda4a3aab3afe
Tobias Lindgaard <tobias.private@gmail.com>
2011-12-06 22:31:35 +0100

    Changes

--- lottery.c
+++ lottery.c
@@ -1,22 +1,23 @@
-#include <stdio.h>
-#include <time.h>
-#include <stdlib.h>
-int randNum()
-{
-int r = (random()*time(NULL)%49+1);
-return r;
-}
-
+#include "lotto.h"
 
 int main(int argc, char* argv[])
 {
 int num, i;
+int count, j;
 puts("Welcome to this lottery program");
 puts("|======================================|");
+printf("How many times would you like to have this lottery run ?");
+scanf("%d", &count);
+j=0;
+while(count > j)
+{
 for(i=0; i < 6; i++)
 {
 num = randNum();
 printf("%d ", abs(num));
+}
+printf("\n");
+j++;
 }
 printf("\n");
 return 0;
